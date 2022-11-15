9496c6af0cd0d9b1647f48701e1166285d0b8b05
dolanske <dolanovsky@gmail.com>
2022-11-15 13:56:36 +0200

    Full coverage of $.get

--- src/__test__/get.test.ts
+++ src/__test__/get.test.ts
@@ -21,9 +21,22 @@ test('Return nodes from current chain', async () => {
   expect(returnSpans).toStrictEqual([span1, span2])
 })
 
-test('Return specific Element propert', async () => {
+test('Return specific Element property', async () => {
   const div = document.createElement('div')
   div.id = 'test'
   const returnDiv = await $(div).get('id')
   expect(returnDiv).toBe('test')
+})
+
+test('Return specific attributes from multiple elements', async () => {
+  const span1 = document.createElement('span')
+  span1.title = 'Hello'
+  const span2 = document.createElement('span')
+  span2.title = 'World'
+
+  document.body.appendChild(span1)
+  document.body.appendChild(span2)
+
+  const titles = await $(document.body).children().get('title')
+  expect(titles).toStrictEqual(['Hello', 'World'])
 })
