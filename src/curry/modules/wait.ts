80dd68fda7c8581f8b1f06c38c7d779f1cb01080
dolanske <dolanovsky@gmail.com>
2022-11-15 14:33:51 +0200

    Use delay utility function inside $.wait instead of a custom one

--- src/curry/modules/wait.ts
+++ src/curry/modules/wait.ts
@@ -1,8 +1,17 @@
 import type { Curry } from '..'
+import { delay } from '../util'
 
 export type Wait = (this: Curry, timeout: number) => Curry
 
+/**
+ * Method used to pause execution chain
+ *
+ * @param this Curry instace
+ * @param timeout Length of pause in miliseconds
+ * @returns Curry instance for chaining
+ */
+
 export const _wait: Wait = function (this, timeout = 1) {
-  this.queue(() => new Promise(resolve => setTimeout(resolve, timeout)))
+  this.queue(() => delay(timeout))
   return this
 }
