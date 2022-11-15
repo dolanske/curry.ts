9689a2facc138d6341d308372ace0d1d4e1ae560
dolanske <dolanovsky@gmail.com>
2022-11-15 14:25:21 +0200

    Slight refactor to $.is, add full coverage

--- src/__test__/is.test.ts
+++ src/__test__/is.test.ts
@@ -4,21 +4,26 @@
 
 import { describe, expect, test } from 'vitest'
 import { $ } from '../curry/index'
-// TODO: rewrite after function refactor
 
 describe('Conditional element matching', () => {
-  test.skip('Single condition', async () => {
+  test('Single condition', async () => {
     const div = document.createElement('div')
     div.id = 'test'
-    expect($(div).is('#test')).toBe(true)
-    expect($(div).is('span')).toBe(false)
+
+    expect($(div).is('#test')).toBeTruthy()
+    expect($(div).is('span')).toBeFalsy()
+    // @ts-expect-error Empty param check
+    expect($(div).is()).toBeFalsy()
   })
-  test.skip('Multiple condition and apply types', async () => {
+
+  test('Multiple condition and apply types', async () => {
     const div = document.createElement('div')
     div.id = 'id'
     div.classList.add('class')
-    expect($(div).is(['#id', '.class'], 'every')).toBe(true)
-    expect($(div).is(['#id', '.non-class'], 'some')).toBe(false)
-    expect($(div).is(['span', 'h1'])).toBe(false)
+
+    expect($(div).is(['#id', '.class'])).toBeTruthy()
+    expect($(div).is(['#id', '.non-class'], 'some')).toBeTruthy()
+    expect($(div).is(['span', 'h1'])).toBeFalsy()
+    expect($(div).is(['span', 'h1'], 'none')).toBeTruthy()
   })
 })
--- src/curry/modules/is.ts
+++ src/curry/modules/is.ts
@@ -14,46 +14,23 @@ export type Is = (
  * @returns Results of the condition check
  */
 
-// TODO:
-// Decide if applyTo should be relevant to the condition
-// or matched elements
-
-// NOTE:
-// applyTo could be used for condition matching instead of elements
-// and $.are could be the same as $.is but applyTo would be for a list
-
-// $.is(["#id", ".class"], "some")
-// NOTE: this means at least one of the conditions applies ot the FIRST element in the selected list
-
-// $.are(["#id", ".class"])
-// NOTE: returns true if every matched element matches the condition
-
-// $.none(["#id", ".class"])
-// Returns true if none of the matched elements match the condition
-
-export const _is: Is = function (condition, applyTo = 'some') {
+export const _is: Is = function (condition, applyTo = 'every') {
   const results: boolean[] = []
 
   for (const _node of this.nodes) {
     const el = toEl(_node)
 
-    if (isArray(condition))
-      results.push(condition.every((c: string) => el.matches(c)))
-    else if (el.matches && el.matches(condition))
-      results.push(true)
-    else
-      results.push(false)
+    if (isArray(condition)) {
+      for (const _condition of condition)
+        results.push(el.matches && el.matches(_condition))
+    }
+    else if (el.matches && el.matches(condition)) { results.push(true) }
+    else { results.push(false) }
   }
 
   switch (applyTo) {
-    case 'some': {
-      return results.some(r => r)
-    }
-    case 'every': {
-      return results.every(r => r)
-    }
-    case 'none': {
-      return !results.some(r => r)
-    }
+    case 'some': return results.some(r => r)
+    case 'none': return !results.some(r => r)
+    case 'every': return results.every(r => r)
   }
 }
