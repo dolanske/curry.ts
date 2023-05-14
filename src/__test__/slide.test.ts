20e107c35ba28ab68bade3bf843dc05c71c43193
dolanske <dolanovsky@gmail.com>
2023-05-15 01:13:55 +0300

    wip: test

--- src/__test__/slide.test.ts
+++ src/__test__/slide.test.ts
@@ -2,9 +2,8 @@
  * @vitest-environment jsdom
  */
 
-import { describe, expect, test } from "vitest";
-import { $ } from "../curry";
-import { delay, formatPrefixAttr } from "../curry/util";
+import { describe, expect, test } from 'vitest'
+import { $ } from '../curry'
 
 describe('$.slide() methods', () => {
   test('slideDown', async () => {
@@ -13,11 +12,11 @@ describe('$.slide() methods', () => {
 
     const start = performance.now()
 
+    // FIXME: animation does not wait
     $(document.body.children[0]).slideUp(300).run(() => {
       expect(performance.now() - start).toBeGreaterThanOrEqual(300)
       expect(el.style.getPropertyValue('display')).toBe('none')
       expect(el.style.getPropertyValue('overflow')).toBe('hidden')
     })
-
   })
 })
