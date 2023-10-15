7559b0230084bf6e8371e94a53309a3a93dbf546
Jan Dolanský <dolanovsky@gmail.com>
2023-10-15 11:30:08 +0200

    wip

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+- [] For all observers, change the callback parameters to a single destructured object. Include `self` for cases where users use arrow functions
+
+- [] Implement view transitions API once their support lands
---
+++ src/curry/modules/transition.ts
@@ -0,0 +1,96 @@
+/**
+ * API for view transition
+ */
+
+import { $, Curry } from ".."
+
+type TransitionLifecycle = (transition: ViewTransitionInstance) => void
+
+interface TransitionCallback {
+  self: Node
+  /**
+   * Runs once the pseudo-element tree is created and the transition animation is about to start.
+   */
+  onReady: TransitionLifecycle
+  /**
+   * Runs once the transition animation is finished, and the new page view is visible and interactive to the user.
+   */
+  onFinished: TransitionLifecycle
+  /**
+   * Runs when the promise returned by the document.startViewTransition()'s callback fulfills.
+   */
+  onCallbackUpdate: TransitionLifecycle
+}
+
+type Transition = (
+  this: Curry,
+  cb: ({}: TransitionCallback) => void
+)=> Curry
+
+/**
+ * Transition the state of the DOM to the newly created state inside the callback.
+ * 
+ * @param this Curry instance
+ * @param cb Callback in which you can create the updated DOM state
+ * @returns Curry instance for chaining
+ */
+export const _transition: Transition = function(this, cb) {
+  this.queue(() => {
+    if (!('startViewTransition' in document)) {
+      return console.warn("Unsupported API - View Transition")
+    }
+
+    const onReadyCbs:TransitionLifecycle[] = []
+    const onFinishedCbs:TransitionLifecycle[] = []
+    const onCallbackUpdateCbs: TransitionLifecycle[] = []
+
+    const onReady = (cb: TransitionLifecycle) => onReadyCbs.push(cb)
+    const onFinished = (cb: TransitionLifecycle) => onFinishedCbs.push(cb)
+    const onCallbackUpdate = (cb: TransitionLifecycle) => onCallbackUpdateCbs.push(cb)
+
+    const transitionInstance = document.startViewTransition(() => {
+      // Callback stuff
+      for (const node of this.nodes) {
+        
+        cb.call(node, {
+          onReady,
+          onFinished,
+          onCallbackUpdate,
+          self: node
+        })
+      }
+    });
+
+    // Can be used to start the custom transition
+    transitionInstance.ready.then(() => {
+      onReadyCbs.forEach(cb => cb(transitionInstance))
+    })
+
+    transitionInstance.finished.then(() => {
+      onFinishedCbs.forEach(cb => cb(transitionInstance))
+    })
+
+    transitionInstance.updateCallbackDone.then(() => {
+      onCallbackUpdateCbs.forEach(cb => cb(transitionInstance))
+    })
+  })
+
+  return this
+}
+
+
+// Exmple usage
+// $('elemenet').transition(function({ onReady, onFinished, onCallbackUpdate }) {
+//   // Update the ELEMENT in any way
+//   $(this).css('color', 'blue')
+
+//   // Callback for when transition is ready
+//   onReady((transition) => {
+//     transition.stop()
+//   })
+
+//   // Callback when transition completes
+//   onFinished(() => {
+
+//   })
+// })
